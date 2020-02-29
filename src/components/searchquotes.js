9dba65f3d768a30c58f4901f7c9bd3f8843602c7
Rojab Khan <irojabkhan@gmail.com>
2020-03-01 02:14:33 +0600

    Initial Commit

---
+++ src/components/searchquotes.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+import '../App.css';
+
+
+
+function SearchQuote() {
+
+  const [quotes, setQuote] = useState([])
+
+
+  axios.get('https://quote-garden.herokuapp.com/quotes/all')
+    .then(res => {
+      setQuote(res.data.results)
+    })
+    .catch(err => console.log(err))
+
+  return (
+    <div className="container">
+      <div className="quotes">
+        {quotes.map(singleQuote => {
+          return (
+            <div className="quote" key={singleQuote._id}>
+              <h3>{singleQuote.quoteText}</h3>
+              <p>- {singleQuote.quoteAuthor}</p>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+
+export default SearchQuote;
